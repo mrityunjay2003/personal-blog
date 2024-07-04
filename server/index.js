4b6be379ba155b6000e639109a91e9cfb1c6dedc
mrityunjay2003 <mrityunjay281203@gmail.com>
2024-07-04 20:19:18 +0530

    fixing cors

--- server/index.js
+++ server/index.js
@@ -11,10 +11,12 @@ const mainRoutes = require('./routes/main');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 connectDB();
-
-app.use(cors());
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}
+app.use(cors(corsOptions));
 
 
 app.use(express.urlencoded({ extended: true }));
